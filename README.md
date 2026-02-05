3859c7a8a7af75210fe659192372f017ac99c4cc
Aman <aman.kumar@softsensor.ai>
2026-02-05 18:01:56 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# 100x-dev
+
+Since you already are a 10x dev, why not go one step further and be a 100x dev.
+
+## Features
+This extension provides 2 functions to call via command palette.
+### 100x Transform
+Become 100x instantly
+
+### 100x Revert
+Revert back to uga booga dev form
+
+## Requirements
+Dev should have more than 2 brain cells to use this extension.
+
+## Extension Settings
+You can set your power level and boost level to go even further.
+
+For example (in Settings JSON):
+* `1000x.powerLevel`: 20000,
+* `1000x.boostLevel`: 40000,
+
+
+> If you can't tell, this is sarcasm. Real 100x devs uses Emacs.
---
+++ extension.js
@@ -0,0 +1,60 @@
+const vscode = require("vscode");
+
+let intervalId = null;
+
+function randomInt(min = 10, max = min) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+async function handel100xing() {
+  if (!vscode.window.activeTextEditor) return;
+
+  const tabs = vscode.window.tabGroups.all
+    .flatMap((t) => t.tabs)
+    .filter((t) => t.input instanceof vscode.TabInputText);
+  if (tabs.length < 2)
+    return void vscode.window.showInformationMessage("Not enough tabs");
+
+  const currIdx = tabs.findIndex((t) => t.isActive);
+  const nextIdx = (currIdx + randomInt(1, 4)) % tabs.length;
+
+  await vscode.window.showTextDocument(tabs[nextIdx].input.uri, {
+    preview: true,
+  });
+}
+
+async function start100xing(boost) {
+  const randomInterval = randomInt(2000, 7000);
+  clearInterval(intervalId);
+  intervalId = setInterval(handel100xing, randomInterval);
+}
+
+function activate(context) {
+  const startCommand = vscode.commands.registerCommand("100x.transform", () => {
+    const config = vscode.workspace.getConfiguration("100x");
+    const power = config.get("powerLevel") || 5000;
+    const boost = config.get("boostLevel") || 5000;
+
+    if (intervalId) clearInterval(intervalId);
+    intervalId = setInterval(() => start100xing(boost), power);
+  });
+
+  const stopCommand = vscode.commands.registerCommand("100x.revert", () => {
+    if (intervalId) {
+      clearInterval(intervalId);
+      intervalId = null;
+    }
+  });
+
+  context.subscriptions.push(startCommand);
+  context.subscriptions.push(stopCommand);
+}
+
+function deactivate() {
+  if (intervalId) clearInterval(intervalId);
+}
+
+module.exports = {
+  activate,
+  deactivate,
+};
